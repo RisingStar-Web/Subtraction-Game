718c79fe1e3ee7188de45ff4e602302af4535075
syavah <syavah@gmail.com>
2019-03-14 00:30:31 +0200

    Initial Commit

---
+++ Subtraction Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Subtraction Game
+//
+//  Created by Yaroslav Sarnitskiy on 3/14/19.
+//  Copyright © 2019 GS Develop Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
