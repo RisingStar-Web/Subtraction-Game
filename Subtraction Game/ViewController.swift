548af00773ff3d43d4f7db99c8d250cb857bd628
syavah <syavah@gmail.com>
2019-03-15 13:37:14 +0200

    Connection main mode

--- Subtraction Game/ViewController.swift
+++ Subtraction Game/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblQuestion: UILabel!
+    
+    @IBOutlet weak var lblTotalCorrect: UILabel!
+    
+    @IBOutlet weak var lblCorrectIncorrect: UILabel!
+    
+    @IBOutlet weak var btnAnswer0: UIButton!
+    @IBOutlet weak var btnAnswer1: UIButton!
+    @IBOutlet weak var btnAnswer2: UIButton!
+    @IBOutlet weak var btnAnswer3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func btnAnswer0ACTION(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func btnAnswer1ACTION(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func btnAnswer2ACTION(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func btbAnswer3ACTION(_ sender: UIButton) {
+        
+    }
+    
 }
 
